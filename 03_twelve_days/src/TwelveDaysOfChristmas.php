a501c2a20fc7875f42cb644d37e73e40c4ca126a
jamieappleyard <jamie.appleyard@forgehg.cloud>
2024-11-22 16:00:03 +0000

    Twelve days of christmas solution

--- 03_twelve_days/src/TwelveDaysOfChristmas.php
+++ 03_twelve_days/src/TwelveDaysOfChristmas.php
@@ -2,7 +2,58 @@
 
 namespace App;
 
+use InvalidArgumentException;
+
 class TwelveDaysOfChristmas
 {
 
+    CONST DAY_LINES = [
+        1 => ['First', 'partridge in a pear tree'],
+        2 => ['Second', 'Two turtle doves'],
+        3 => ['Third', 'Three French hens'],
+        4 => ['Fourth', 'Four calling birds'],
+        5 => ['Fifth', 'Five golden rings'],
+        6 => ['Sixth', 'Six geese a-laying'],
+        7 => ['Seventh', 'Seven swans a-swimming'],
+        8 => ['Eighth', 'Eight maids a-milking'],
+        9 => ['Ninth', 'Nine ladies dancing'],
+        10 => ['Tenth', 'Ten lords a-leaping'],
+        11 => ['Eleventh', 'Eleven pipers piping'],
+        12 => ['Twelfth', 'Twelve drummers drumming'],
+    ];
+
+    public function daysOfChristmas(int $day)
+    {
+
+        if ($day > 12 || $day < 1) {
+            throw new InvalidArgumentException("Day $day must be between 1 and 12 inclusive");
+        }
+
+        $dayLine = self::DAY_LINES[$day];
+
+        $song = "On the $dayLine[0] day of Christmas, My true love gave to me:";
+
+        if ($day === 1) {
+            return $song . "\n A " . $dayLine[1];
+        }
+
+        $finalSong = $this->addDaysOfChristmas($song, $day);
+
+        return $finalSong;
+    }
+
+    private function addDaysOfChristmas(string $song, int $currentDay=null): string
+    {
+        if ($currentDay > 1) {
+            $dayLine = self::DAY_LINES[$currentDay];
+
+            $song = $song . "\n" . $dayLine[1] . ',';
+
+            $song = $this->addDaysOfChristmas($song, $currentDay-1);
+        } else {
+            $song = $song . "\nAnd a " . self::DAY_LINES[1][1] . '.';
+        }
+
+        return $song;
+    }
 }
--- 03_twelve_days/tests/TwelveDaysOfChristmasTest.php
+++ 03_twelve_days/tests/TwelveDaysOfChristmasTest.php
@@ -2,14 +2,67 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use InvalidArgumentException;
+use App\TwelveDaysOfChristmas;
+use TypeError;
 
-#[CoversNothing]
+#[CoversClass(TwelveDaysOfChristmas::class)]
 class TwelveDaysOfChristmasTest extends TestCase
 {
-    public function test_the_truth(): void
+    private TwelveDaysOfChristmas $twelveDaysOfChristmas;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->twelveDaysOfChristmas = new TwelveDaysOfChristmas();
+    }
+
+    public function test_tdoc_returns_string_when_passed_int(): void
+    {
+        $this->assertIsString($this->twelveDaysOfChristmas->daysOfChristmas(1));
+    }
+
+    public function test_tdoc_returns_error_when_not_passed_int(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->twelveDaysOfChristmas->daysOfChristmas('1');
+    }
+
+    public function test_tdoc_returns_error_when_passed_int_outside_1_12(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->twelveDaysOfChristmas->daysOfChristmas(-1);
+        $this->twelveDaysOfChristmas->daysOfChristmas(13);
+    }
+
+    public function test_tdoc_returns_first_verse_and_day_lines(): void
+    {
+
+        $expected = "On the First day of Christmas, My true love gave to me:\n A partridge in a pear tree";
+        $result = $this->twelveDaysOfChristmas->daysOfChristmas(1);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_tdoc_returns_full_song_when_passed_12(): void
+    {
+        $expected = "On the Twelfth day of Christmas, My true love gave to me:\nTwelve drummers drumming,
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.";
+
+        $result = $this->twelveDaysOfChristmas->daysOfChristmas(12);
+
+        $this->assertEquals($expected, $result);
     }
 }
